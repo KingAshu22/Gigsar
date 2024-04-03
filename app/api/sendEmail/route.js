1ec6d8c5499276ed840b0d0c0e7d8108ddede058
KingAshu22 <ashishprasad9833@gmail.com>
2024-04-03 19:01:17 +0530

    removed sendEmail for now

--- app/api/sendEmail/route.js
+++ app/api/sendEmail/route.js
@@ -1,25 +1,25 @@
-import { Resend } from 'resend';
-import { NextResponse } from 'next/server';
-import EmailTemplate from '@/emails';
+// import { Resend } from 'resend';
+// import { NextResponse } from 'next/server';
+// import EmailTemplate from '@/emails';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST(req){
+// export async function POST(req){
 
-    const response=await req.json()
-    const result=response.data;
-    try{
+//     const response=await req.json()
+//     const result=response.data;
+//     try{
 
-        const data=await resend.emails.send({
-            from: 'Doctor-Appointment-Booking@tubeguruji-app.tubeguruji.com',
-            to: [response.data.Email],
-            subject: 'Appointment Booking Confirmation',
-            react: EmailTemplate({result})
-          });
-        return NextResponse.json({data})
-    }
-    catch(error)
-    {
-        return NextResponse.json({error})
-    }
-}
+//         const data=await resend.emails.send({
+//             from: 'Doctor-Appointment-Booking@tubeguruji-app.tubeguruji.com',
+//             to: [response.data.Email],
+//             subject: 'Appointment Booking Confirmation',
+//             react: EmailTemplate({result})
+//           });
+//         return NextResponse.json({data})
+//     }
+//     catch(error)
+//     {
+//         return NextResponse.json({error})
+//     }
+// }

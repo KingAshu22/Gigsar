e3245ff6a16274f7b72511cdd4a5b97c639d6d4d
KingAshu22 <ashishprasad9833@gmail.com>
2024-04-03 19:03:47 +0530

    Removed Booking Components for now

--- app/(route)/my-booking/_components/CancelAppointment.jsx
+++ app/(route)/my-booking/_components/CancelAppointment.jsx
@@ -1,41 +1,40 @@
-import React from 'react'
-import {
-    AlertDialog,
-    AlertDialogAction,
-    AlertDialogCancel,
-    AlertDialogContent,
-    AlertDialogDescription,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogTitle,
-    AlertDialogTrigger,
-  } from "@/components/ui/alert-dialog"
-import { Button } from '@/components/ui/button'
-  
-function CancelAppointment({onContinueClick}) {
+// import React from 'react'
+// import {
+//     AlertDialog,
+//     AlertDialogAction,
+//     AlertDialogCancel,
+//     AlertDialogContent,
+//     AlertDialogDescription,
+//     AlertDialogFooter,
+//     AlertDialogHeader,
+//     AlertDialogTitle,
+//     AlertDialogTrigger,
+//   } from "@/components/ui/alert-dialog"
+// import { Button } from '@/components/ui/button'
 
-    
-  return (
-    <AlertDialog>
-    <AlertDialogTrigger>
-    <Button variant="outline" className="text-primary border-primary">Cancel Appointment</Button>
-    </AlertDialogTrigger>
-    <AlertDialogContent>
-      <AlertDialogHeader>
-        <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-        <AlertDialogDescription>
-          This action cannot be undone. This will permanently delete your appointment
-          and remove your data from our servers.
-        </AlertDialogDescription>
-      </AlertDialogHeader>
-      <AlertDialogFooter>
-        <AlertDialogCancel>Cancel</AlertDialogCancel>
-        <AlertDialogAction onClick={()=>onContinueClick()}>Continue</AlertDialogAction>
-      </AlertDialogFooter>
-    </AlertDialogContent>
-  </AlertDialog>
-  
-  )
-}
+// function CancelAppointment({onContinueClick}) {
 
-export default CancelAppointment
+//   return (
+//     <AlertDialog>
+//     <AlertDialogTrigger>
+//     <Button variant="outline" className="text-primary border-primary">Cancel Appointment</Button>
+//     </AlertDialogTrigger>
+//     <AlertDialogContent>
+//       <AlertDialogHeader>
+//         <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+//         <AlertDialogDescription>
+//           This action cannot be undone. This will permanently delete your appointment
+//           and remove your data from our servers.
+//         </AlertDialogDescription>
+//       </AlertDialogHeader>
+//       <AlertDialogFooter>
+//         <AlertDialogCancel>Cancel</AlertDialogCancel>
+//         <AlertDialogAction onClick={()=>onContinueClick()}>Continue</AlertDialogAction>
+//       </AlertDialogFooter>
+//     </AlertDialogContent>
+//   </AlertDialog>
+
+//   )
+// }
+
+// export default CancelAppointment

43ab378351ede469ee423acd149ca89d2555dc6a
KingAshu22 <ashishprasad9833@gmail.com>
2024-05-20 19:56:53 +0530

    updated the navbar

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,18 +1,23 @@
 "use client";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { useEffect, useState } from "react";
 import Link from "next/link";
-import React, { useEffect } from "react";
 import { UserButton, useUser } from "@clerk/nextjs";
-import { CircleUserRound } from "lucide-react";
+import { AlignJustify, CircleUserRound } from "lucide-react";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import { Button } from "@/components/ui/button";
 
 function Header() {
   const { user } = useUser();
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
   const Menu = [
     {
       id: 1,
@@ -30,45 +35,54 @@ function Header() {
     //   path: "/",
     // },
   ];
+
   return (
-    <div
-      className="flex items-center 
-    justify-between p-4 shadow-sm"
-    >
+    <div className="flex items-center justify-between p-4 shadow-sm bg-white">
       <div className="flex items-center gap-10">
         <Link href="/">
           <h1 className="font-bold text-4xl text-primary">Gigsar</h1>
         </Link>
-        <ul className="md:flex gap-8 hidden">
+        <ul className="hidden md:flex gap-8">
           {Menu.map((item, index) => (
             <Link href={item.path} key={index}>
-              <li
-                className="hover:text-primary
-                    cursor-pointer hover:scale-105
-                    transition-all ease-in-out"
-              >
+              <li className="hover:text-primary cursor-pointer hover:scale-105 transition-all ease-in-out">
                 {item.name}
               </li>
             </Link>
           ))}
         </ul>
-        <ul className="md:flex gap-8 hidden">
-          {user ? (
-            <UserButton afterSignOutUrl="/sign-in" />
-          ) : (
-            <Link href="/sign-in">
-              <CircleUserRound />
-            </Link>
-          )}
-          {user ? (
-            <UserButton afterSignOutUrl="/sign-in" />
-          ) : (
-            <Link href="/sign-in">
-              <CircleUserRound />
-            </Link>
-          )}
-        </ul>
       </div>
+      {isMounted && (
+        <div className="md:hidden flex items-center gap-8 md:justify-end">
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button variant="ghost" className="p-0">
+                <AlignJustify className="w-6 h-6" />
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent>
+              <ul className="flex flex-col gap-4 p-2">
+                {Menu.map((item, index) => (
+                  <Link href={item.path} key={index}>
+                    <li className="hover:text-primary cursor-pointer hover:scale-105 transition-all ease-in-out">
+                      {item.name}
+                    </li>
+                  </Link>
+                ))}
+              </ul>
+            </PopoverContent>
+          </Popover>
+          <div className="md:hidden">
+            {user ? (
+              <UserButton afterSignOutUrl="/sign-in" />
+            ) : (
+              <Link href="/sign-in">
+                <CircleUserRound className="w-6 h-6" />
+              </Link>
+            )}
+          </div>
+        </div>
+      )}
     </div>
   );
 }

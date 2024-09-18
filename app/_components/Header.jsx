a2e40c8f741ced0b9388ce2363ecf8c6cf1b82d7
KingAshu22 <ashishprasad9833@gmail.com>
2024-09-18 13:59:41 +0530

    updated user profile view

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -3,6 +3,8 @@ import React, { useEffect, useState, useCallback } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { AlignJustify, LogOut, MessagesSquare } from "lucide-react";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+
 import {
   Popover,
   PopoverContent,
@@ -10,15 +12,75 @@ import {
 } from "@/components/ui/popover";
 import { Button } from "@/components/ui/button";
 import useAuth from "@/lib/hook";
+import axios from "axios";
+import Modal from "./Modal";
 
 const Header = () => {
+  const [mobileNumber, setMobileNumber] = useState("");
   const [isMounted, setIsMounted] = useState(false);
-  const isAuthenticated = useAuth();
+  const [shortName, setShortName] = useState("");
+  const [client, setClient] = useState({});
+  const [showProfile, setShowProfile] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(useAuth());
   const router = useRouter();
+
+  useEffect(() => {
+    const storedNumber = localStorage?.getItem("mobile");
+    console.log("Stored Number:", storedNumber);
+
+    if (storedNumber !== undefined) {
+      setMobileNumber(JSON?.parse(storedNumber));
+    }
+  }, [isAuthenticated]);
+
+  useEffect(() => {
+    if (mobileNumber) {
+      getClient();
+    }
+  }, [mobileNumber]);
+
+  const getClient = async () => {
+    try {
+      const response = await axios.get(
+        `${process.env.NEXT_PUBLIC_API}/client/contact/+${mobileNumber}`
+      );
+
+      if (response.data) {
+        const name = response.data.name;
+
+        console.log(response.data);
+
+        // Get initials (e.g., KP for Krishna Pandey)
+        const initials = name
+          .split(" ") // Split the name into an array of words
+          .map((word) => word[0]) // Take the first letter of each word
+          .join("") // Join the letters to form initials
+          .toUpperCase(); // Ensure it's in uppercase
+
+        setShortName(initials); // Set the short name as initials
+        setClient(response.data);
+      }
+    } catch (error) {
+      console.error("Error fetching Client:", error);
+    } finally {
+    }
+  };
 
   useEffect(() => {
     setIsMounted(true);
   }, []);
+
+  useEffect(() => {
+    setInterval(() => {
+      const mobile = localStorage.getItem("mobile");
+      const authExpiry = localStorage.getItem("authExpiry");
+      if (mobile && authExpiry && Date.now() < parseInt(authExpiry, 10)) {
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+    }, []);
+  }, 2000);
 
   const handleSignOut = useCallback(() => {
     localStorage.removeItem("mobile");
@@ -34,66 +96,117 @@ const Header = () => {
   ];
 
   return (
-    <div className="flex items-center justify-between p-4 shadow-sm bg-white">
-      <div className="flex items-center gap-10">
-        <Link href="/">
-          <h1 className="font-bold text-4xl text-primary">Gigsar</h1>
-        </Link>
-        <ul className="hidden md:flex gap-8">
-          {Menu.map((item, index) => (
-            <Link href={item.path} key={index}>
-              <li className="hover:text-primary cursor-pointer hover:scale-105 transition-all ease-in-out">
-                {item.name}
-              </li>
-            </Link>
-          ))}
-        </ul>
+    <>
+      <div className="flex items-center justify-between p-4 shadow-sm bg-white">
+        <div className="flex items-center gap-10">
+          <Link href="/">
+            <h1 className="font-bold text-4xl text-primary">Gigsar</h1>
+          </Link>
+          <ul className="hidden md:flex gap-8">
+            {Menu.map((item, index) => (
+              <Link href={item.path} key={index}>
+                <li className="hover:text-primary cursor-pointer hover:scale-105 transition-all ease-in-out">
+                  {item.name}
+                </li>
+              </Link>
+            ))}
+          </ul>
+        </div>
+        {isMounted && (
+          <div className="flex items-center gap-8 md:justify-end">
+            <a href={"/chat"}>
+              <MessagesSquare />
+            </a>
+            <Popover className="md:hidden">
+              <PopoverTrigger asChild>
+                <Button variant="ghost" className="p-0 md:hidden">
+                  <AlignJustify className="w-6 h-6" />
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent>
+                <ul className="flex flex-col gap-4 p-2">
+                  {Menu.map((item, index) => (
+                    <Link href={item.path} key={index}>
+                      <li className="hover:text-primary cursor-pointer hover:scale-105 transition-all ease-in-out">
+                        {item.name}
+                      </li>
+                    </Link>
+                  ))}
+                </ul>
+              </PopoverContent>
+            </Popover>
+            <div>
+              {isAuthenticated && (
+                <Avatar
+                  onClick={() => {
+                    setShowProfile(true);
+                  }}
+                >
+                  <AvatarFallback>{shortName}</AvatarFallback>
+                </Avatar>
+              )}
+            </div>
+          </div>
+        )}
       </div>
-      {isMounted && (
-        <div className="flex items-center gap-8 md:justify-end">
-          <a href={"/chat"}>
-            <MessagesSquare />
-          </a>
-          <Popover className="md:hidden">
+      <Modal
+        isOpen={showProfile}
+        onClose={() => {
+          setShowProfile(false);
+        }}
+        title="Artist Details"
+      >
+        <div className="flex flex-col items-center space-y-6 p-6 relative">
+          {/* Profile Picture */}
+          <div className="relative">
+            <Avatar className="w-24 h-24 border-2 border-gray-300 shadow-lg">
+              <AvatarFallback className="bg-gray-200 text-xl font-bold">
+                {shortName}
+              </AvatarFallback>
+            </Avatar>
+          </div>
+
+          {/* Artist Details */}
+          <div className="text-center space-y-2">
+            <h3 className="text-2xl font-semibold text-gray-800">
+              {client?.name}
+            </h3>
+            <p className="text-sm text-gray-500">
+              <span className="font-semibold">Role</span>:{" "}
+              {client?.type
+                ?.split("-")
+                .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+                .join(" ")}
+            </p>
+            <p className="text-sm text-gray-500">
+              {" "}
+              <span className="font-semibold">Mobile</span>: {client?.contact}
+            </p>
+            <p className="text-sm text-gray-500">
+              {" "}
+              <span className="font-semibold">Email</span>: {client?.email}
+            </p>
+          </div>
+
+          {/* Sign Out Button */}
+          <Popover>
             <PopoverTrigger asChild>
-              <Button variant="ghost" className="p-0 md:hidden">
-                <AlignJustify className="w-6 h-6" />
+              <Button variant="ghost" className="bg-primary text-white">
+                <LogOut className="w-6 h-6" /> Sign Out
               </Button>
             </PopoverTrigger>
             <PopoverContent>
-              <ul className="flex flex-col gap-4 p-2">
-                {Menu.map((item, index) => (
-                  <Link href={item.path} key={index}>
-                    <li className="hover:text-primary cursor-pointer hover:scale-105 transition-all ease-in-out">
-                      {item.name}
-                    </li>
-                  </Link>
-                ))}
-              </ul>
+              <div className="p-4">
+                <p>Are you sure you want to sign out?</p>
+                <Button onClick={handleSignOut} className="mt-2">
+                  Sign Out
+                </Button>
+              </div>
             </PopoverContent>
           </Popover>
-          <div>
-            {isAuthenticated && (
-              <Popover>
-                <PopoverTrigger asChild>
-                  <Button variant="ghost" className="p-0">
-                    <LogOut className="w-6 h-6" />
-                  </Button>
-                </PopoverTrigger>
-                <PopoverContent>
-                  <div className="p-4">
-                    <p>Are you sure you want to sign out?</p>
-                    <Button onClick={handleSignOut} className="mt-2">
-                      Sign Out
-                    </Button>
-                  </div>
-                </PopoverContent>
-              </Popover>
-            )}
-          </div>
         </div>
-      )}
-    </div>
+      </Modal>
+    </>
   );
 };
 

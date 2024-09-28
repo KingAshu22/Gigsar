3509496a6b6754234b3f57b878052c799a9ca457
KingAshu22 <ashishprasad9833@gmail.com>
2024-09-28 17:02:19 +0530

    made function gloabal

--- app/_components/ArtistList.jsx
+++ app/_components/ArtistList.jsx
@@ -92,15 +92,15 @@ function ArtistList({
     }
   }, [selectedEventType]);
 
+  const formatString = (str) => {
+    return str
+      .split("-") // Split by hyphen
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1)) // Capitalize the first letter of each word
+      .join(" "); // Join words with a space
+  };
+
   const sendEnquiry = async (linkid, budget) => {
     if (!linkid) return;
-
-    const formatString = (str) => {
-      return str
-        .split("-") // Split by hyphen
-        .map((word) => word.charAt(0).toUpperCase() + word.slice(1)) // Capitalize the first letter of each word
-        .join(" "); // Join words with a space
-    };
 
     try {
       await axios.post(
